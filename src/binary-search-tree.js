5dc10f84868fbb20244233e933bc3608f6376811
Darya <darya.drazdouskaya@gmail.com>
2021-10-18 22:27:19 +0300

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,103 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 module.exports = class BinarySearchTree {
-
+  constructor() {
+    this._root = null;
+  }
+  // return root node of the tree
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // add node with data to the tree
+  add(data) {
+    let _add = function(root, data) {
+      // если значения root нет
+      if (!root) {
+        return new Node(data);
+        // если значение рута совпадает с пришедшим значением
+      } else if (root.data === data) {
+        return root;
+      }
+      
+      if (data < root.data) {
+        // рекурсия (матрешка) - с каждой итерацией меняем условия старта функции - назначение нового левого рута
+        root.left = _add(root.left, data);
+      } else {
+        root.right = _add(root.right, data);
+      }
+      return root;
+    }
+    this._root = _add(this._root, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // returns true if node with the data exists in the tree and false otherwise
+  has(data) {
+    // '!!' - перевод в boolean, т.к. null = false. Альтернатива тернарному оператору
+    return !!this.find(data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // returns node with the data if node with the data exists in the tree and null otherwise
+  find(data) {
+    let _find = function(root, data) {
+      if (!root) {
+        return null;
+      }
+
+      if (data < root.data) {
+        return _find(root.left, data);
+      } else if (data > root.data) {
+        return _find(root.right, data);
+        // если равны
+      } else {
+        return root;
+      }
+    }
+
+    return _find(this._root, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // removes node with the data from the tree if node with the data exists
+  remove(data) {
   }
 
+  // returns minimal value stored in the tree (or null if tree has no nodes)
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) {
+      return null;
+    }
+
+    let node = this._root;
+
+    // самое маленькое число - самое крайнее слева
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
+  // returns maximal value stored in the tree (or null if tree has no nodes)
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) {
+      return null;
+    }
+
+    let node = this._root;
+
+    // самое большое число - самое крайнее справа
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 
 }
